9b09b0d22f708a7f032353cfe8a9d0c6c805f043
zondel <zikriadirahman@gmail.com>
2026-02-27 09:19:29 +0700

    Update Index.lua

--- Index.lua
+++ Index.lua
@@ -160,6 +160,6 @@ CreateAutoLoadTab("Pabrik", "Memuat otomatis sistem Pabrik.", "https://raw.githu
 CreateAutoLoadTab("Auto Farm", "Sistem farming resource (Kayu, Batu).", "https://raw.githubusercontent.com/Koziz/CAW-SCRIPT/refs/heads/main/Autofarm.lua") 
 CreateAutoLoadTab("Manager", "Sistem Inventory & Sortir Barang.", "https://raw.githubusercontent.com/ZonHUBs/ZONHUB/refs/heads/main/manager.lua")
 CreateAutoLoadTab("Auto Chat", "Auto Chat.", "https://raw.githubusercontent.com/ZonHUBs/ZONHUB/refs/heads/main/autochat.lua")
-CreateAutoLoadTab("terbang", "Sistem Inventory & Sortir Barang.", "https://raw.githubusercontent.com/zikriadirahman-commits/ZONHUB/refs/heads/main/autoclear.lua")
+CreateAutoLoadTab("clear", "Sistem Inventory & Sortir Barang.", "https://raw.githubusercontent.com/zikriadirahman-commits/ZONHUB/refs/heads/main/autoclear.lua")
 CreateAutoLoadTab("anti", "Auto Chat.", "https://raw.githubusercontent.com/zikriadirahman-commits/ZONHUB/refs/heads/main/anti.lua")
 
